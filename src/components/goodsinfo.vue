a932108b3b2963f681d7fe2fbbb1d71935b5202d
lixu <lx18653917010@163.com>
2021-01-16 17:35:05 +0800

    jkdjckahjkdfh

--- src/components/goodsinfo.vue
+++ src/components/goodsinfo.vue
@@ -5,12 +5,12 @@
 
       <p> 商品信息</p>
       <div class="inp" style="border-bottom: ipx solid black;">
-         <div class="input-group mb-3" style="width: 30%;margin: auto;">
-            <input type="text" class="form-control"v-model="uname" placeholder="请输入商品名或关键字"style="background:rgba(255,255,255,0.1);" >
-            <div class="input-group-append">
-              <span class="ss input-group-text btn">搜索</span>
-            </div>
+        <div class="input-group mb-3" style="width: 30%;margin: auto;">
+          <input type="text" class="form-control" v-model="uname" placeholder="请输入商品名或关键字" style="background:rgba(255,255,255,0.1);">
+          <div class="input-group-append">
+            <span class="ss input-group-text btn">搜索</span>
           </div>
+        </div>
       </div>
       <table class="table">
         <tr class="bg-warning">
@@ -44,7 +44,7 @@
           <li class="page-item"><a class="page-link" href="#">首页</a></li>
           <li class="page-item"><a class="page-link" href="#">上一页</a></li>
           <li class="page-item active"><a class="page-link" href="#">{{}}</a></li>
-          <li class="page-item"><a class="page-link" href="#" >下一页</a></li>
+          <li class="page-item"><a class="page-link" href="#">下一页</a></li>
           <li class="page-item"><a class="page-link" href="#">尾页</a></li>
         </ul>
       </div>
@@ -64,11 +64,43 @@
     },
     data: function() {
       return {
-          goods:""
+        goods: "",
+        zy:"",
+        pn:[]
       }
     },
-    methods:{
+    methods: {
+      //获取登录状态
+      success: function() {
+        var o = this;
+        $.ajax({
+          url: "http://127.0.0.1:8056/back/ajax/administratoronline",
+          success: function(r) {
+           o.selectgoodsinfo();
 
+          },
+          xhrFields: {
+            withCredentials: true //传递cookie,保持session的唯一性
+          },
+          crossDomain: true,
+        })
+      },
+      //查商品
+      selectgoodsinfo: function() {
+        var o = this;
+        $.ajax({
+          url: "http://127.0.0.1:8080/java06/ajax/selectgoodsinfo",
+          data: {
+                goodsinfo:o.goods
+          },
+          success: function(r) {
+                 o.goods=r.goods;
+                 o.zy=r.zy;
+                 o.pn=r.pn;
+          }
+        })
+
+      }
     },
     mounted() {
 
@@ -77,21 +109,24 @@
 </script>
 
 <style scoped="">
-.goods {
+  .goods {
     width: 85%;
     float: right;
     height: 920px;
-    background-color:#E0FFFF ;
+    background-color: #E0FFFF;
     display: inline-block;
   }
-  p{
-    background-color:moccasin;
+
+  p {
+    background-color: moccasin;
     padding: 15px;
   }
-  .ss:active{
-border: 1px solid #8FBC8F;
+
+  .ss:active {
+    border: 1px solid #8FBC8F;
   }
-  .ss:hover{
-  background-color: burlywood;
-    }
+
+  .ss:hover {
+    background-color: burlywood;
+  }
 </style>
